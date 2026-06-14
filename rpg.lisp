d3efee84248552df63f02526234008e39c081418
vinin2379 <pasta.arq@gmail.com>
2026-06-14 16:24:21 -0300

    receba

--- rpg.lisp
+++ rpg.lisp
@@ -28,16 +28,20 @@
 
 ;; ====== 3. ESTADO GLOBAL ======
 
-(defvar *clima-atual*        "Normal")
-(defvar *descricao-clima*    "Funcoes em estado padrao. Danos fixos e previstos.")
-(defvar *log-combate*        (list "O combate ainda nao comecou!"))
-(defvar *max-linhas-log*     8)
+(defvar *clima-atual* "Normal")
+(defvar *descricao-clima* "Funcoes em estado padrao. Danos fixos e previstos.")
+(defvar *log-combate* (list "O combate ainda nao comecou!"))
+(defvar *max-linhas-log* 8)
+
+;; Nossos personagens agora sao GLOBAIS para podermos hackear pelo REPL!
+(defvar *guerreiro* nil)
+(defvar *mago* nil)
+(defvar *chefe* nil)
 
 (defun adicionar-log (msg)
   (setf *log-combate* (append *log-combate* (list msg)))
   (when (> (length *log-combate*) *max-linhas-log*)
     (setf *log-combate* (cdr *log-combate*))))
-
 ;; ====== 4. MÚLTIPLO DESPACHO ======
 
 (defgeneric atacar (atacante alvo))
@@ -126,97 +130,56 @@
 
 (defun iniciar-jogo ()
   (setf *random-state* (make-random-state t))
-  (setf *log-combate*  (list "O combate ainda nao comecou!"))
+  (setf *log-combate* (list "O combate ainda nao comecou!"))
 
-  (let ((guerreiro (make-instance 'guerreiro :nome "Jones"
-                     :hp 100 :hp-max 100 :forca 15 :magia 0))
-        (mago      (make-instance 'mago :nome "Lisbon"
-                     :hp 80  :hp-max 80  :forca 5  :magia 20))
-        (chefe     (make-instance 'monstro :nome "Prof. de Paradigmas"
-                     :hp 300 :hp-max 300))
-        (turno 0))  ;; 0=guerreiro ataca, 1=mago ataca, 2=boss ataca guerreiro, 3=boss ataca mago
+  ;; Inicializando as variaveis globais em vez de usar 'let'
+  (setf *guerreiro* (make-instance 'guerreiro :nome "Jones" :hp 100 :hp-max 100 :forca 15 :magia 0))
+  (setf *mago* (make-instance 'mago :nome "Lisbon" :hp 80  :hp-max 80  :forca 5  :magia 20))
+  (setf *chefe* (make-instance 'monstro :nome "Prof. de Paradigmas" :hp 300 :hp-max 300))
+
+  (let ((turno 0))  ;; Turno continua local, pois nao precisamos hackear ele
 
     (ltk:with-ltk ()
       (ltk:wm-title ltk:*tk* "Candy Box: Lisp Mutante Edition")
 
       (let* (
              ;; === TITULO ===
-             (lbl-titulo (make-instance 'ltk:label
-                           :text "=== CANDY BOX: LISP MUTANTE ==="
-                           :font "Courier 14 bold"))
+             (lbl-titulo (make-instance 'ltk:label :text "=== CANDY BOX: LISP MUTANTE ===" :font "Courier 14 bold"))
 
              ;; === CLIMA ===
-             (lbl-clima (make-instance 'ltk:label
-                          :text (format nil "Clima: ~A" *clima-atual*)
-                          :font "Helvetica 11 bold"
-                          :foreground "darkgreen"))
-             (lbl-desc-clima (make-instance 'ltk:label
-                               :text *descricao-clima*
-                               :font "Helvetica 9 italic"
-                               :foreground "gray30"))
+             (lbl-clima (make-instance 'ltk:label :text (format nil "Clima: ~A" *clima-atual*) :font "Helvetica 11 bold" :foreground "darkgreen"))
+             (lbl-desc-clima (make-instance 'ltk:label :text *descricao-clima* :font "Helvetica 9 italic" :foreground "gray30"))
 
              ;; === STATUS DOS PERSONAGENS ===
              (lbl-status-g (make-instance 'ltk:label
-                             :text (format nil "Guerreiro ~A: ~A"
-                                           (nome guerreiro)
-                                           (barra-hp (hp guerreiro) (hp-max guerreiro)))
-                             :font "Courier 10"
-                             :foreground "navy"))
+                             :text (format nil "Guerreiro ~A: ~A" (nome *guerreiro*) (barra-hp (hp *guerreiro*) (hp-max *guerreiro*)))
+                             :font "Courier 10" :foreground "navy"))
              (lbl-status-m (make-instance 'ltk:label
-                             :text (format nil "Mago      ~A: ~A"
-                                           (nome mago)
-                                           (barra-hp (hp mago) (hp-max mago)))
-                             :font "Courier 10"
-                             :foreground "purple"))
+                             :text (format nil "Mago      ~A: ~A" (nome *mago*) (barra-hp (hp *mago*) (hp-max *mago*)))
+                             :font "Courier 10" :foreground "purple"))
              (lbl-status-b (make-instance 'ltk:label
-                             :text (format nil "Boss ~A: ~A"
-                                           (nome chefe)
-                                           (barra-hp (hp chefe) (hp-max chefe)))
-                             :font "Courier 10 bold"
-                             :foreground "darkred"))
+                             :text (format nil "Boss ~A: ~A" (nome *chefe*) (barra-hp (hp *chefe*) (hp-max *chefe*)))
+                             :font "Courier 10 bold" :foreground "darkred"))
 
-             ;; === LOG DE COMBATE (Text multilinha, readonly) ===
-             (lbl-log-titulo (make-instance 'ltk:label
-                               :text "-- Log de Combate --"
-                               :font "Helvetica 10 bold"))
-             (txt-log (make-instance 'ltk:text
-                        :width 65 :height 8))
+             ;; === LOG DE COMBATE ===
+             (lbl-log-titulo (make-instance 'ltk:label :text "-- Log de Combate --" :font "Helvetica 10 bold"))
+             (txt-log (make-instance 'ltk:text :width 65 :height 8))
 
-             ;; === AST DAS FUNCOES (Text multilinha, readonly) ===
-             (lbl-ast-titulo (make-instance 'ltk:label
-                               :text "-- AST das Funcoes na RAM (fdefinition ao vivo) --"
-                               :font "Courier 10 bold"))
-             (lbl-ast-g (make-instance 'ltk:label
-                          :text "calcular-dano-guerreiro:"
-                          :font "Courier 9 bold"
-                          :foreground "navy"))
+             ;; === AST DAS FUNCOES ===
+             (lbl-ast-titulo (make-instance 'ltk:label :text "-- AST das Funcoes na RAM (fdefinition ao vivo) --" :font "Courier 10 bold"))
+             (lbl-ast-g (make-instance 'ltk:label :text "calcular-dano-guerreiro:" :font "Courier 9 bold" :foreground "navy"))
              (txt-ast-guerreiro (make-instance 'ltk:text :width 65 :height 3))
-             (lbl-ast-m (make-instance 'ltk:label
-                          :text "calcular-dano-mago:"
-                          :font "Courier 9 bold"
-                          :foreground "purple"))
+             (lbl-ast-m (make-instance 'ltk:label :text "calcular-dano-mago:" :font "Courier 9 bold" :foreground "purple"))
              (txt-ast-mago (make-instance 'ltk:text :width 65 :height 3))
-             (lbl-ast-b (make-instance 'ltk:label
-                          :text "calcular-dano-boss:"
-                          :font "Courier 9 bold"
-                          :foreground "darkred"))
+             (lbl-ast-b (make-instance 'ltk:label :text "calcular-dano-boss:" :font "Courier 9 bold" :foreground "darkred"))
              (txt-ast-boss (make-instance 'ltk:text :width 65 :height 3))
 
              ;; === FUNCOES DE ATUALIZACAO DA UI ===
              (atualizar-status
                (lambda ()
-                 (setf (ltk:text lbl-status-g)
-                       (format nil "Guerreiro ~A: ~A"
-                               (nome guerreiro)
-                               (barra-hp (hp guerreiro) (hp-max guerreiro))))
-                 (setf (ltk:text lbl-status-m)
-                       (format nil "Mago      ~A: ~A"
-                               (nome mago)
-                               (barra-hp (hp mago) (hp-max mago))))
-                 (setf (ltk:text lbl-status-b)
-                       (format nil "Boss ~A: ~A"
-                               (nome chefe)
-                               (barra-hp (hp chefe) (hp-max chefe))))))
+                 (setf (ltk:text lbl-status-g) (format nil "Guerreiro ~A: ~A" (nome *guerreiro*) (barra-hp (hp *guerreiro*) (hp-max *guerreiro*))))
+                 (setf (ltk:text lbl-status-m) (format nil "Mago      ~A: ~A" (nome *mago*) (barra-hp (hp *mago*) (hp-max *mago*))))
+                 (setf (ltk:text lbl-status-b) (format nil "Boss ~A: ~A" (nome *chefe*) (barra-hp (hp *chefe*) (hp-max *chefe*))))))
 
              (atualizar-log
                (lambda ()
@@ -236,12 +199,9 @@
 
              (atualizar-ast
                (lambda ()
-                 (funcall escrever-ast txt-ast-guerreiro
-                          (format nil "~A" (function-lambda-expression #'calcular-dano-guerreiro)))
-                 (funcall escrever-ast txt-ast-mago
-                          (format nil "~A" (function-lambda-expression #'calcular-dano-mago)))
-                 (funcall escrever-ast txt-ast-boss
-                          (format nil "~A" (function-lambda-expression #'calcular-dano-boss)))))
+                 (funcall escrever-ast txt-ast-guerreiro (format nil "~A" (function-lambda-expression #'calcular-dano-guerreiro)))
+                 (funcall escrever-ast txt-ast-mago (format nil "~A" (function-lambda-expression #'calcular-dano-mago)))
+                 (funcall escrever-ast txt-ast-boss (format nil "~A" (function-lambda-expression #'calcular-dano-boss)))))
 
              ;; === BOTAO PRINCIPAL ===
              (btn-turno
@@ -249,32 +209,29 @@
                  :text ">>> AVANCAR TURNO <<<"
                  :command (lambda ()
                             (cond
-                              ;; Todos os herois mortos
-                              ((and (<= (hp guerreiro) 0) (<= (hp mago) 0))
+                              ((and (<= (hp *guerreiro*) 0) (<= (hp *mago*) 0))
                                (adicionar-log "[DERROTA] Os herois cairam... O Professor vence!")
                                (funcall atualizar-log))
-                              ;; Boss derrotado
-                              ((<= (hp chefe) 0)
+                              ((<= (hp *chefe*) 0)
                                (adicionar-log "[VITORIA] O chefe foi derrotado! A prova foi entregue!")
                                (funcall atualizar-log))
-                              ;; Combate normal: turno ciclico
                               (t
                                (cond
                                  ((= turno 0)
-                                  (if (> (hp guerreiro) 0)
-                                    (atacar guerreiro chefe)
+                                  (if (> (hp *guerreiro*) 0)
+                                    (atacar *guerreiro* *chefe*)
                                     (adicionar-log "[SKIP] Guerreiro esta morto.")))
                                  ((= turno 1)
-                                  (if (> (hp mago) 0)
-                                    (atacar mago chefe)
+                                  (if (> (hp *mago*) 0)
+                                    (atacar *mago* *chefe*)
                                     (adicionar-log "[SKIP] Mago esta morto.")))
                                  ((= turno 2)
-                                  (if (> (hp guerreiro) 0)
-                                    (atacar chefe guerreiro)
+                                  (if (> (hp *guerreiro*) 0)
+                                    (atacar *chefe* *guerreiro*)
                                     (adicionar-log "[SKIP] Boss tenta atacar Guerreiro, mas ele ja caiu.")))
                                  ((= turno 3)
-                                  (if (> (hp mago) 0)
-                                    (atacar chefe mago)
+                                  (if (> (hp *mago*) 0)
+                                    (atacar *chefe* *mago*)
                                     (adicionar-log "[SKIP] Boss tenta atacar Mago, mas ele ja caiu."))))
                                (setf turno (mod (+ turno 1) 4))
                                (funcall atualizar-status)
@@ -283,75 +240,66 @@
 
              ;; === BOTOES DE CLIMA ===
              (btn-normal
-               (make-instance 'ltk:button
-                 :text "[1] Clima Normal"
+               (make-instance 'ltk:button :text "[1] Clima Normal"
                  :command (lambda ()
                             (mudar-clima-do-mundo 1)
-                            (setf (ltk:text lbl-clima)      (format nil "Clima: ~A" *clima-atual*))
+                            (setf (ltk:text lbl-clima) (format nil "Clima: ~A" *clima-atual*))
                             (setf (ltk:text lbl-desc-clima) *descricao-clima*)
                             (adicionar-log (format nil "[CLIMA] Mudou para: ~A" *clima-atual*))
                             (funcall atualizar-log)
                             (funcall atualizar-ast))))
 
              (btn-fogo
-               (make-instance 'ltk:button
-                 :text "[2] Clima Infernal"
+               (make-instance 'ltk:button :text "[2] Clima Infernal"
                  :command (lambda ()
                             (mudar-clima-do-mundo 2)
-                            (setf (ltk:text lbl-clima)      (format nil "Clima: ~A" *clima-atual*))
+                            (setf (ltk:text lbl-clima) (format nil "Clima: ~A" *clima-atual*))
                             (setf (ltk:text lbl-desc-clima) *descricao-clima*)
                             (adicionar-log (format nil "[CLIMA] Mudou para: ~A" *clima-atual*))
                             (funcall atualizar-log)
                             (funcall atualizar-ast))))
 
              (btn-antimagia
-               (make-instance 'ltk:button
-                 :text "[3] Zona Antimagia"
+               (make-instance 'ltk:button :text "[3] Zona Antimagia"
                  :command (lambda ()
                             (mudar-clima-do-mundo 3)
-                            (setf (ltk:text lbl-clima)      (format nil "Clima: ~A" *clima-atual*))
+                            (setf (ltk:text lbl-clima) (format nil "Clima: ~A" *clima-atual*))
                             (setf (ltk:text lbl-desc-clima) *descricao-clima*)
                             (adicionar-log (format nil "[CLIMA] Mudou para: ~A" *clima-atual*))
                             (funcall atualizar-log)
                             (funcall atualizar-ast))))
 
              (btn-inversao
-               (make-instance 'ltk:button
-                 :text "[4] Inversao Total"
+               (make-instance 'ltk:button :text "[4] Inversao Total"
                  :command (lambda ()
                             (mudar-clima-do-mundo 4)
-                            (setf (ltk:text lbl-clima)      (format nil "Clima: ~A" *clima-atual*))
+                            (setf (ltk:text lbl-clima) (format nil "Clima: ~A" *clima-atual*))
                             (setf (ltk:text lbl-desc-clima) *descricao-clima*)
                             (adicionar-log (format nil "[CLIMA] Mudou para: ~A" *clima-atual*))
                             (funcall atualizar-log)
                             (funcall atualizar-ast)))))
 
         ;; === LAYOUT ===
-        (ltk:pack lbl-titulo        :pady 10)
-        (ltk:pack lbl-clima         :pady 3)
-        (ltk:pack lbl-desc-clima    :pady 2)
-
-        (ltk:pack lbl-status-g      :anchor :w :padx 40 :pady 1)
-        (ltk:pack lbl-status-m      :anchor :w :padx 40 :pady 1)
-        (ltk:pack lbl-status-b      :anchor :w :padx 40 :pady 1)
-
-        (ltk:pack btn-turno         :fill :x :padx 40 :pady 8)
-
-        (ltk:pack btn-normal        :fill :x :padx 40 :pady 2)
-        (ltk:pack btn-fogo          :fill :x :padx 40 :pady 2)
-        (ltk:pack btn-antimagia     :fill :x :padx 40 :pady 2)
-        (ltk:pack btn-inversao      :fill :x :padx 40 :pady 2)
-
-        (ltk:pack lbl-log-titulo    :pady 6)
-        (ltk:pack txt-log           :padx 40 :pady 2)
-
-        (ltk:pack lbl-ast-titulo    :pady 8)
-        (ltk:pack lbl-ast-g         :anchor :w :padx 40)
+        (ltk:pack lbl-titulo :pady 10)
+        (ltk:pack lbl-clima :pady 3)
+        (ltk:pack lbl-desc-clima :pady 2)
+        (ltk:pack lbl-status-g :anchor :w :padx 40 :pady 1)
+        (ltk:pack lbl-status-m :anchor :w :padx 40 :pady 1)
+        (ltk:pack lbl-status-b :anchor :w :padx 40 :pady 1)
+        (ltk:pack btn-turno :fill :x :padx 40 :pady 8)
+        (ltk:pack btn-normal :fill :x :padx 40 :pady 2)
+        (ltk:pack btn-fogo :fill :x :padx 40 :pady 2)
+        (ltk:pack btn-antimagia :fill :x :padx 40 :pady 2)
+        (ltk:pack btn-inversao :fill :x :padx 40 :pady 2)
+        (ltk:pack lbl-log-titulo :pady 6)
+        (ltk:pack txt-log :padx 40 :pady 2)
+        (ltk:pack lbl-ast-titulo :pady 8)
+        (ltk:pack lbl-ast-g :anchor :w :padx 40)
         (ltk:pack txt-ast-guerreiro :padx 40 :pady 2)
-        (ltk:pack lbl-ast-m         :anchor :w :padx 40)
-        (ltk:pack txt-ast-mago      :padx 40 :pady 2)
-        (ltk:pack lbl-ast-b         :anchor :w :padx 40)
-        (ltk:pack txt-ast-boss      :padx 40 :pady 2)
+        (ltk:pack lbl-ast-m :anchor :w :padx 40)
+        (ltk:pack txt-ast-mago :padx 40 :pady 2)
+        (ltk:pack lbl-ast-b :anchor :w :padx 40)
+        (ltk:pack txt-ast-boss :padx 40 :pady 2)
 
         ;; Inicializa tudo
         (ltk:after 100
